d8ac0e6d422652b0d1207a1f1a0403c6670ce143
Paul Taylor <hello@ampersandwich.co.uk>
2016-03-12 04:56:32 +0000

    Restoration.

--- src/ReflectRouter.js
+++ src/ReflectRouter.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import Route from './Route.js';
+import RouteCollection from './RouteCollection.js';
+import Dispatch from './Dispatch.js';
+import Middleware from './Middleware.js';
 
 class Router extends React.Component {
     constructor(props) {
         super(props);
 
-        this.displayName = 'ReflectRouter';
+        this.name = 'Router';
 
         window.addEventListener('popstate', this.locationChange.bind(this), false);
+
+        this.RouteCollection    = new RouteCollection(null);
+        this.Dispatch           = new Dispatch;
+        this.Middleware         = new Middleware;
 
         return this;
     }
@@ -18,10 +26,19 @@ class Router extends React.Component {
             ':uuid': '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
         };
 
+        this.RouteCollection.create(this.props.children);
+        this.Dispatch.assign(this).do();
+
         return this;
     }
 
     locationChange() {
+        this.Dispatch.History.check();
+
+        this.setState({
+            route: this.Dispatch.click
+        });
+
         return this;
     }
 
@@ -39,11 +56,23 @@ class Router extends React.Component {
     }
 
     render() {
-        return (<span>Reflect Router</span>);
+        if(typeof this.Dispatch.currentRoute === 'undefined') {
+            return (<span></span>);
+        }
+
+        var current = this.Dispatch.currentRoute;
+
+        if(this.Middleware.use(current.Route)) {
+            return (<this.Middleware.component />);
+        };
+
+        return (<current.component />);
     }
 }
 
 Router.propTypes = {};
 Router.defaultProps = {};
 
-export default { Router };
+Router.displayName = 'ReflectRouter';
+
+export { Router, Route };
